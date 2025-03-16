92623df3963bbb5fd90df037aa671809b762f183
yulee <dbfl0461@gmail.com>
2025-03-16 21:54:11 +0900

    Refactor: 사용하지 않는 import 제거 #7

--- back-end/reacton/src/main/java/com/softeer/reacton/domain/course/controller/StudentCourseController.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/domain/course/controller/StudentCourseController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
