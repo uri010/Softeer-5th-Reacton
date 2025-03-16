b71506b7171a6d0b3b6c96c01e5d22d6a1843012
yulee <dbfl0461@gmail.com>
2025-03-16 21:02:11 +0900

    Feat: ArgumentResovler 등록 #7

--- back-end/reacton/src/main/java/com/softeer/reacton/global/config/WebConfig.java
+++ back-end/reacton/src/main/java/com/softeer/reacton/global/config/WebConfig.java
@@ -1,13 +1,32 @@
 package com.softeer.reacton.global.config;
 
+import com.softeer.reacton.global.jwt.AccessTokenArgumentResolver;
+import com.softeer.reacton.global.jwt.SignupTokenArgumentResolver;
+import com.softeer.reacton.global.jwt.StudentTokenArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final SignupTokenArgumentResolver signupTokenArgumentResolver;
+    private final AccessTokenArgumentResolver accessTokenArgumentResolver;
+    private final StudentTokenArgumentResolver studentTokenArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(signupTokenArgumentResolver);
+        resolvers.add(accessTokenArgumentResolver);
+        resolvers.add(studentTokenArgumentResolver);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
